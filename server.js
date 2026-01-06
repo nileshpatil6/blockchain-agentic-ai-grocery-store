76d3a6c2fdb4bf7f047721053a47cd9d0edf872a
nileshpatil6 <technil6436@gmail.com>
2026-01-06 23:32:11 +0530

    feat: configure server to use environment variable for PORT and add render configuration

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
